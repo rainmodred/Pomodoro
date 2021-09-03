b80f1fa8027e4228dbbeda4dfb3601d912a63e7f
rainmodred <albphoenix@gmail.com>
2021-09-03 22:41:46 +0300

    Add timer styles

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,12 +1,29 @@
+import styles from './Clock.module.css';
+
 interface ClockProps {
   id: string;
   time: number;
 }
 
 function timeToMinSec(time: number) {
-  return new Date(time * 1000).toISOString().slice(14, 19);
+  const [minutes, seconds] = new Date(time * 1000)
+    .toISOString()
+    .slice(14, 19)
+    .split(':');
+
+  return (
+    <>
+      {minutes}
+      <span className={styles.semicolon}>:</span>
+      {seconds}
+    </>
+  );
 }
 
 export default function Clock({ id, time }: ClockProps): JSX.Element {
-  return <div data-testid={`${id}-clock`}>{timeToMinSec(time)}</div>;
+  return (
+    <div className={styles.clock} data-testid={`${id}-clock`}>
+      {timeToMinSec(time)}
+    </div>
+  );
 }
