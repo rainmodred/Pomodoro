32c347c02c68fafdb46616eb46c797909a7890df
rainmodred <albphoenix@gmail.com>
2021-09-03 23:29:57 +0300

    Update styles

--- src/components/ProgressRing/ProgressRing.tsx
+++ src/components/ProgressRing/ProgressRing.tsx
@@ -17,7 +17,7 @@ export default function ProgressRing({
   const circumference = normalizedRadius * 2 * Math.PI;
   const strokeDashoffset = circumference - (progress / 100) * circumference;
   return (
-    <svg height={radius * 2} width={radius * 2}>
+    <svg height={radius * 2} width={radius * 2} className="progressRing">
       <circle
         stroke={color}
         fill="transparent"
