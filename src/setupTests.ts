8c183dec843c1f3f96d3dd84c15491e04e4f1530
rainmodred <albphoenix@gmail.com>
2022-01-26 02:22:34 +0300

    Mock notification in tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -7,3 +7,11 @@ import '@testing-library/jest-dom';
 jest
   .spyOn(window.HTMLMediaElement.prototype, 'pause')
   .mockImplementation(() => {});
+
+//beforeALl not working
+global.beforeEach(() => {
+  global.Notification = {
+    requestPermission: jest.fn().mockResolvedValue('denied'),
+    permission: 'granted',
+  } as unknown as jest.Mocked<typeof Notification>;
+});
