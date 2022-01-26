30b389944b24ef604950cae7f2e841193d52414c
rainmodred <albphoenix@gmail.com>
2022-01-27 02:39:43 +0300

    Fix e2e tests

--- cypress/integration/pomodoro.spec.ts
+++ cypress/integration/pomodoro.spec.ts
@@ -1,6 +1,8 @@
+//TODO: Add more tests
+
 describe('pomodoro app', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:5000');
   });
 
   it('displays heading and pomodoro timer by default', () => {
@@ -13,7 +15,7 @@ describe('pomodoro app', () => {
       name: /start/i,
     }).click();
     cy.tick(2000);
-    cy.findByTestId('pomodoro-clock').should('have.text', '24:58');
+    cy.findByTestId('clock').should('have.text', '24:58');
   });
 
   it('can reset timer', () => {
@@ -22,16 +24,16 @@ describe('pomodoro app', () => {
       name: /start/i,
     }).click();
     cy.tick(2000);
-    cy.findByTestId('pomodoro-reset').click();
-    cy.findByTestId('pomodoro-clock').should('have.text', '25:00');
+    cy.findByTestId('reset').click();
+    cy.findByTestId('clock').should('have.text', '25:00');
   });
 
   it('can change tab', () => {
-    cy.findByRole('tab', {
+    cy.findByRole('button', {
       name: /short break/i,
     }).click();
 
-    cy.findByTestId('short break-clock').should('have.text', '05:00');
+    cy.findByTestId('clock').should('have.text', '05:00');
   });
 
   it('timer resets on tab change', () => {
@@ -40,17 +42,16 @@ describe('pomodoro app', () => {
       name: /start/i,
     }).click();
     cy.tick(2000);
-    cy.findByRole('tab', {
+    cy.findByRole('button', {
       name: /short break/i,
     }).click();
-    cy.findByRole('tab', {
+    cy.findByRole('button', {
       name: /pomodoro/i,
     }).click();
-    cy.findByTestId('pomodoro-clock').should('have.text', '25:00');
+    cy.findByTestId('clock').should('have.text', '25:00');
   });
 
   it('can change settings', () => {
-    expect(localStorage.getItem('settings')).to.equal(null);
     cy.findByTestId('settings').click();
     cy.findByRole('spinbutton', {
       name: /pomodoro/i,
@@ -73,26 +74,23 @@ describe('pomodoro app', () => {
           JSON.parse(window.localStorage.getItem('settings')),
         ).to.deep.equal({
           timers: [
-            {
-              label: 'pomodoro',
-              time: 1800,
-            },
-            {
-              label: 'short break',
-              time: 600,
-            },
-            {
-              label: 'long break',
-              time: 600,
-            },
+            { label: 'pomodoro', time: 1800 },
+            { label: 'short break', time: 600 },
+            { label: 'long break', time: 600 },
           ],
           selectedColor: '#d880f5',
+          sound: {
+            name: 'Analog Alarm',
+            volume: 50,
+          },
+          autostart: true,
+          notification: true,
         });
       });
-    cy.findByTestId('pomodoro-clock').should('have.text', '30:00');
-    cy.findByRole('tab', {
+    cy.findByTestId('clock').should('have.text', '30:00');
+    cy.findByRole('button', {
       name: /short break/i,
     }).click();
-    cy.findByTestId('short break-clock').should('have.text', '10:00');
+    cy.findByTestId('clock').should('have.text', '10:00');
   });
 });
